b75f43f656fbfae21cae0e773d8dd73cc0e7dccc
Carlos Emilio Carbajal Nogues <a01373264@iMac-de-administrador-5.local>
2019-02-27 17:09:08 -0600

    request

--- app/src/main/java/mx/itesm/cecnn/practica_redes_carlos/DescargaTextoFrag.java
+++ app/src/main/java/mx/itesm/cecnn/practica_redes_carlos/DescargaTextoFrag.java
@@ -6,12 +6,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.StringRequestListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DescargaTextoFrag extends Fragment {
+
+    private TextView tvContenido;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        tvContenido=getView().findViewById(R.id.tvContenido);
+        AndroidNetworking.initialize(getContext());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        String url="https://www.gutenberg.org/cache/epub/2000/pg2000.txt";
+        AndroidNetworking.get(url).build().getAsString(new StringRequestListener() {
+            @Override
+            public void onResponse(String response) {
+                tvContenido.setText(response);
+            }
+
+            @Override
+            public void onError(ANError anError) {
+                tvContenido.setText("Error en la descarga: "+anError.getErrorCode());
+            }
+        });
+
+    }
 
 
     public DescargaTextoFrag() {
